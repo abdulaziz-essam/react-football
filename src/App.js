32b851dd35de0d4f3f50821924560bcfea9451c5
aek20 <66400022+aek20@users.noreply.github.com>
2022-02-19 00:27:05 +0300

    18 feb

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from '../src/components/navbar/Navbar.js'
+import {Button,Row,Col} from "react-bootstrap"
 function App() {
   return (
     <div className="App">
-hi
+ <Navbar/>
     </div>
   );
 }
